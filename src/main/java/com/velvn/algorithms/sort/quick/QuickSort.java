fa35b4d26f6542171c715177c6bd4d9ed8e6921f
ynatt <thecruly@gmail.com>
2018-09-28 00:32:32 +0400

    Change quick sort algorithm.

--- src/main/java/com/velvn/algorithms/sort/quick/QuickSort.java
+++ src/main/java/com/velvn/algorithms/sort/quick/QuickSort.java
@@ -3,7 +3,7 @@ package com.velvn.algorithms.sort.quick;
 import com.velvn.algorithms.generator.ArrayGenerator;
 import com.velvn.algorithms.sort.SortAlgorithm;
 
-public class QuickSort extends SortAlgorithm{
+public class QuickSort extends SortAlgorithm {
 
 
     public QuickSort(ArrayGenerator generator) {
@@ -16,27 +16,33 @@ public class QuickSort extends SortAlgorithm{
 
     @Override
     public void execute() {
-        sort(0,array.length - 1);
+        sort(0, array.length - 1);
     }
 
-    private void sort(int start, int end){
-        if(start < end){
-            int middle = partition(start, end);
-            sort(start,middle - 1);
-            sort(middle + 1,end);
+    private void sort(int start, int end) {
+        if (start < end) {
+            int middle = randomPartition(start, end);
+            sort(start, middle - 1);
+            sort(middle + 1, end);
         }
     }
 
-    private int partition(int start, int end){
-        int pivot = array[end/2];
+    private int randomPartition(int start, int end) {
+        int i = (int) Math.round(Math.random() * (end - start));
+        swap(end, i + start);
+        return partition(start, end);
+    }
+
+    private int partition(int start, int end) {
+        int pivot = array[end];
         int i = start - 1;
-        for(int j = start; j < end; j++){
-            if(array[j] <= pivot){
-               i++;
-               swap(i,j);
+        for (int j = start; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(i, j);
             }
         }
-        swap(i + 1, end/2);
+        swap(i + 1, end);
         return i + 1;
     }
 
